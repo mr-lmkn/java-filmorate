7a0910b57a1682009e4c010648ae9774c4094886
SLomakin <sergey.lmkn@yandex.ru>
2024-01-20 19:12:19 +0300

    Sprint 10.4
    - Проверяю, что изменения проходят тесты GitHub

--- src/main/java/ru/yandex/practicum/filmorate/validation/MinReleaseDateConstraintValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/MinReleaseDateConstraintValidator.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.validation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
