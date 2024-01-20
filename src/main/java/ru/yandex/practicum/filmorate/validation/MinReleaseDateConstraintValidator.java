fe8691e511a906edeb0b0401d8338de6432a2bfd
SLomakin <sergey.lmkn@yandex.ru>
2024-01-20 18:54:36 +0300

    Sprint 10.4
    - Проверяю, что изменения проходят тесты GitHub

--- src/main/java/ru/yandex/practicum/filmorate/validation/MinReleaseDateConstraintValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/MinReleaseDateConstraintValidator.java
@@ -16,27 +16,27 @@ import java.time.format.DateTimeFormatter;
 public class MinReleaseDateConstraintValidator implements ConstraintValidator<MinReleaseDateConstraint, LocalDate> {
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd"); // "1895-12-28";
 
-    @Value("${ru.yandex.practicum.filmorate.validation.MIN_FILM_RELEASE_DATE}")
-    private String MIN_FILM_RELEASE_DATE;
+    @Value("${ru.yandex.practicum.filmorate.validation.minReleaseDate}")
+    private String minReleaseDate;
 
     @Override
     public boolean isValid(LocalDate value, ConstraintValidatorContext context) {
-        if (MIN_FILM_RELEASE_DATE == null) {
+        if (minReleaseDate == null) {
             // Я так и не понял как сделать тест для этой валидации, есть идеи как это запустить?
             // Мок тест? Но я, хочу тестировать только валидацию
-            // MIN_FILM_RELEASE_DATE - в тесте у меня всегда NULL
-            MIN_FILM_RELEASE_DATE = "1895-12-28";
+            // minReleaseDate - в тесте у меня всегда NULL
+            minReleaseDate = "1895-12-28";
         }
-        log.debug("Setting 'minReleaseDate' is set to {}", MIN_FILM_RELEASE_DATE);
+        log.debug("Setting 'minReleaseDate' is set to {}", minReleaseDate);
         if (value != null) {
-            log.debug("Compare {} to {}", MIN_FILM_RELEASE_DATE, value.toString());
-            return value.isAfter(LocalDate.parse(MIN_FILM_RELEASE_DATE, formatter));
+            log.debug("Compare {} to {}", minReleaseDate, value.toString());
+            return value.isAfter(LocalDate.parse(minReleaseDate, formatter));
         }
         return true;
     }
 
     public String getMinReleaseDate() {
-        return MIN_FILM_RELEASE_DATE;
+        return minReleaseDate;
     }
 
 }
